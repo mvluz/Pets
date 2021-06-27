da4cf29b691de42910264570cd8e2cbfac86e8ee
mvluz <marcus.i.t@outlook.com>
2021-06-27 02:29:45 -0300

    Calculate.js Sketch and HTML Style Sketch

---
+++ js/calculate.js
@@ -0,0 +1,65 @@
+function resetClick(id){
+    let element = document.getElementById(id);
+    element.style.color='black';
+}
+function calculate() {
+    let age = parseFloat(document.getElementById("dog-age").value);
+    let porte = parseInt(document.getElementById("porte").value);
+    let result, restAge, greaterThanTwo;
+
+    if (age <= 0 || isNaN(age) || age === "") {
+        alert("Idade inválida. Informe um número positivo");
+        document.getElementById("dog-age").focus();
+        return;
+    }
+
+    if (porte == 1) {
+
+        greaterThanTwo = age > 2 ? true : false;
+        if (greaterThanTwo) {
+            restAge = parseFloat(age - 2);
+            restAge = restAge * 4.5;
+            result = restAge + 25;
+        } else {
+            result = age * 12.5;
+        }
+
+    }
+    else {
+        if (porte == 2) {
+
+            greaterThanTwo = age > 2 ? true : false;
+            if (greaterThanTwo) {
+                restAge = parseFloat(age - 2);
+                restAge = restAge * 5.7;
+                result = restAge + 21 ;
+            } else {
+                result = age * 10.5;
+            }
+
+        }
+        else {
+            if (porte == 3) {
+
+                greaterThanTwo = age > 2 ? true : false;
+                if (greaterThanTwo) {
+                    restAge = parseFloat(age - 2);
+                    restAge = restAge * 8;
+                    result = restAge + 18;
+                } else {
+                    result = age * 9;
+                }
+
+            }
+            else {
+                alert("Selecione um tamanho");
+                let element = document.getElementById("porte");
+                element.style.color='red';
+                return;
+            }
+        }
+    }
+
+    document.getElementById("resultado").value = String(result).replace(".",",") + " Anos.";
+    
+}
